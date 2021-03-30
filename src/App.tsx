0a92b9cd7e70c29820b9115d1edc66c2987087ab
nathaniaelvina <nathaniaelvina@hotmail.com>
2021-03-30 18:29:23 +0700

    Remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Daily, {DailyEvent} from '@daily-co/react-native-daily-js';
 
 import {createMeeting} from './av/createMeeting';
 import {getJoinToken} from './av/getjoinToken';
-import {joinCall} from './av/joinCall';
 
 export default function App() {
   let onStartCallPress = async () => {
@@ -12,50 +11,47 @@ export default function App() {
     await createMeeting(meetingId);
     let id = Math.floor(Math.random() * Math.floor(100)).toString();
     let joinToken = await getJoinToken(id, meetingId);
-    await joinCall({
-      joinToken,
-      meetingId,
-      beforeConnect: () => {},
-      listeners: () => [],
+
+    const call = Daily.createCallObject();
+    call.join({
+      url: `https://remixhq.daily.co/${meetingId}`,
+      token: joinToken,
+      // @ts-ignore
+      audioSource: true,
+      // @ts-ignore
+      videoSource: false,
     });
 
-    // const call = Daily.createCallObject();
-    // call.join({
-    //   url: `https://remixhq.daily.co/${meetingId}`,
-    //   token: joinToken,
-    //   // @ts-ignore
-    //   audioSource: true,
-    //   // @ts-ignore
-    //   videoSource: false,
-    // });
-
-    // // Listen for events signaling changes to participants or their audio or video.
-    // // - 'participant-joined' and 'participant-left' are for remote participants only
-    // // - 'participant-updated' is for the local participant as well as remote participants
-    // const events: DailyEvent[] = [
-    //   'participant-joined',
-    //   'participant-updated',
-    //   'participant-left',
-    //   'error',
-    // ];
-    // for (const event of events) {
-    //   call.on(event, () => {
-    //     console.log('EVENT', event);
-    //     for (const participant of Object.values(call.participants())) {
-    //       console.log('---');
-    //       console.log(`participant ${participant.user_id}:`);
-    //       if (participant.local) {
-    //         console.log('is local');
-    //       }
-    //       if (participant.audio) {
-    //         console.log('audio enabled', participant.audioTrack);
-    //       }
-    //       if (participant.video) {
-    //         console.log('video enabled', participant.videoTrack);
-    //       }
-    //     }
-    //   });
-    // }
+    const events: DailyEvent[] = [
+      'participant-joined',
+      'participant-updated',
+      'participant-left',
+      'error',
+    ];
+    for (const event of events) {
+      call.on(event, () => {
+        /**
+         * After pressing the button, wait for a few seconds.
+         * It should throw
+         * Possible Unhandled Promise Rejection (id: 0):
+         * "Failed to load call object bundle https://c.daily.co/static/call-machine-object-bundle.js: SyntaxError: 123:2483153:async functions are unsupported"
+         */
+        console.log('EVENT', event);
+        for (const participant of Object.values(call.participants())) {
+          console.log('---');
+          console.log(`participant ${participant.user_id}:`);
+          if (participant.local) {
+            console.log('is local');
+          }
+          if (participant.audio) {
+            console.log('audio enabled', participant.audioTrack);
+          }
+          if (participant.video) {
+            console.log('video enabled', participant.videoTrack);
+          }
+        }
+      });
+    }
   };
 
   return (
